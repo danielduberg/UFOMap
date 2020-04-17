3ea94ef83637f33d6b2fb38da5d7606905d3f58e
Daniel Duberg <danielduberg@gmail.com>
2020-04-17 13:46:49 +0200

    Added Vector3 (Point)

--- ufomap/include/ufomap/geometry/types.h
+++ ufomap/include/ufomap/geometry/types.h
@@ -8,12 +8,14 @@
 #include <ufomap/geometry/plane.h>
 #include <ufomap/geometry/ray.h>
 #include <ufomap/geometry/sphere.h>
+#include <ufomap/math/vector3.h>
 
 #include <variant>
 
 namespace ufomap_geometry
 {
-using BoundingVar = std::variant<AABB, Frustum, LineSegment, OBB, Plane, Ray, Sphere>;
+using BoundingVar =
+		std::variant<AABB, Frustum, LineSegment, OBB, Vector3, Plane, Ray, Sphere>;
 
 }  // namespace ufomap_geometry
 
